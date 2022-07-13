b81c9c1f3ecaf6dc10dd08041b94006f16529457
Walid ARB <wmadaoui@arb-tg.com>
2022-07-13 15:29:58 -0400

    add nbr of units calculation. uses normal returns instead of log returns

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ import pandas as pd
 import binance_api
 from datetime import datetime, timedelta
 from discord.ext import commands
-from statsmodels.tsa.stattools import OLS, add_constant
+from statsmodels.tsa.stattools import OLS, add_constant, adfuller
 from exchange.ftx.client import FtxClient
 
 # Initialization
@@ -22,15 +22,16 @@ with open(config_path, 'r') as f:
 
 
 @bot.command(name='beta')
-async def get_beta(ctx, asset_a, asset_b="", period="", resolution_minutes=""):
+async def get_beta(ctx, asset_a, asset_b="", period="", resolution_minutes="", dollar_size="100$"):
     if ctx.channel.id == config['channel_ids']['commands_test']:
         if asset_a == 'help':
-            help_msg = 'Usage:\n You want to check the beta between eth and btc based on last 10 days of 5min data \n Example:\n`!beta eth-perp btc-perp 30days 5min`'
-            help_msg += '\nMethodology: Uses OLS Linear Regression. Tries both permutations and returns the mean of beta. \n`beta_mean = (beta_1 + 1/beta_2) / 2`'
+            help_msg = 'Usage:\n You want to check the beta between BTC and ETH based on last 10 days of 5min data and calculate how much units of each based on 2500$ of BTC \n Example:\n`!beta btc-perp eth-perp 30days 5min 2500$`'
+            help_msg += '\nMethodology: Uses OLS Linear Regression. Tries both permutations and returns the beta of the combination that has the lowest pvalue of ADFULLER test'
             await ctx.channel.send(help_msg)
         else:
             period = int(period.replace('days', ''))
             resolution_minutes = int(resolution_minutes.replace('min', ''))
+            dollar_size = float(dollar_size.replace('$', ''))
             # Check if period and resolution match (we have a limit of 5000 datapoints)
             max_period_allowed = int(5000 * resolution_minutes / 60 / 24)
             if period > max_period_allowed:
@@ -49,27 +50,39 @@ async def get_beta(ctx, asset_a, asset_b="", period="", resolution_minutes=""):
                     data_b = pd.DataFrame(client.get_historical_data(asset_b, resolution_seconds, 5000, start_time_ts, end_time_ts))
                     data_a['startTime'] = data_a['startTime'].apply(lambda x: datetime.fromisoformat(x))
                     data_a_returns = data_a['close'].pct_change().dropna()
-                    data_a_log_returns = (data_a_returns + 1).apply(lambda x: math.log(x))
                     data_b['startTime'] = data_b['startTime'].apply(lambda x: datetime.fromisoformat(x))
                     data_b_returns = data_b['close'].pct_change().dropna()
-                    data_b_log_returns = (data_b_returns + 1).apply(lambda x: math.log(x))
 
                     # Calculate correlation
-                    corr = data_a_log_returns.corr(data_b_log_returns, method='pearson')
+                    corr = data_a_returns.corr(data_b_returns, method='pearson')
                     msg = 'Correlation = `{:.2f}`\n'.format(corr)
 
                     # Calculate beta
-                    model = OLS(data_a_log_returns, add_constant(data_b_log_returns))
+                    model = OLS(data_a_returns, add_constant(data_b_returns))
                     reg = model.fit()
                     beta_1 = reg.params[1]
-                    model2 = OLS(data_b_log_returns, add_constant(data_a_log_returns))
+                    pvalue_1 = adfuller(reg.resid)[1]
+                    model2 = OLS(data_b_returns, add_constant(data_a_returns))
                     reg2 = model2.fit()
                     beta_2 = reg2.params[1]
+                    pvalue_2 = adfuller(reg2.resid)[1]
 
-                    beta = (beta_1 + 1/beta_2) / 2
+                    if pvalue_1 < pvalue_2:
+                        beta = beta_1
+                        dollar_size_a = dollar_size
+                        dollar_size_b = dollar_size * beta
+                        units_a = dollar_size_a/data_a.iloc[-1]['close']
+                        units_b = dollar_size_b / data_b.iloc[-1]['close']
+                    else:
+                        beta = beta_2
+                        dollar_size_a = dollar_size * beta
+                        dollar_size_b = dollar_size 
+                        units_a = dollar_size_a/data_a.iloc[-1]['close']
+                        units_b = dollar_size_b / data_b.iloc[-1]['close']
 
                     if beta > 0:
-                        msg += 'Beta = `{:.2f}` \nFor `$100` of `{}`, you need `${:.2f}` of `{}`'.format(beta, asset_a, 100*beta, asset_b)
+                        #msg += 'Beta = `{:.2f}` \nFor `${:.2f}` of `{}`, you need `${:.2f}` of `{}`'.format(beta, asset_a, 100*beta, asset_b)
+                        msg += 'Beta = `{:.2f}` \nFor `{:.2f}` units of `{}` worth `{:.2f}$`, you need `{:.2f}` units of `{}` worth `{:.2f}$`'.format(beta, units_a, asset_a.upper(), dollar_size_a, units_b, asset_b.upper(), dollar_size_b)
                     else:
                         msg += 'Beta is negative, meaning the correlation is probably negative. Not an ideal candidate for a pair trade'
                     await ctx.channel.send(msg)
